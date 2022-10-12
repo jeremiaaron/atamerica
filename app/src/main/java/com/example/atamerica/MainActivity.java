1cc60a93ff7d16e3a39f37c76810be0caaa6ae39
Roger Cashley <rogerOT@outlook.com>
2022-10-12 12:06:49 +0700

    Finished splash splash screen and added colors.

--- app/src/main/java/com/example/atamerica/MainActivity.java
+++ app/src/main/java/com/example/atamerica/MainActivity.java
@@ -1,14 +1,36 @@
 package com.example.atamerica;
 
 import androidx.appcompat.app.AppCompatActivity;
+import android.content.Intent;
+import android.os.Handler;
 
 import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
+import android.content.Intent;
+import android.os.Handler;
 
 public class MainActivity extends AppCompatActivity {
+
+    private int splashScreenDuration = 1000;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        getSupportActionBar().hide();
+
         setContentView(R.layout.activity_main);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent home = new Intent(MainActivity.this, HomePageActivity.class);
+                startActivity(home);
+                finish();
+            }
+        }, splashScreenDuration);
     }
 }
