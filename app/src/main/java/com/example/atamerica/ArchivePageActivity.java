93de20023ac0544383643a6369d7df1e40458f0a
Erwin <septianto.mes@gmail.com>
2022-10-12 23:33:52 +0700

    Ini punya ku ya, tolong di review
    Still confused how to implement back navigation in top and implement "Read More" feature

--- app/src/main/java/com/example/atamerica/ArchivePageActivity.java
+++ app/src/main/java/com/example/atamerica/ArchivePageActivity.java
@@ -6,13 +6,15 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class ArchivePageActivity extends AppCompatActivity {
 
 //    BottomNavigationView bottomNavigationView;
-
+    Button dummy;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
