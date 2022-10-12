aa5fd3be1dcb1194ea649634b5f511b8ad332534
Jeremiah Aaron <jeremiah.aaron117@gmail.com>
2022-10-13 04:48:39 +0700

    Initial upcoming page, updated colors, additional image placeholder asset

--- app/src/main/java/com/example/atamerica/UpcomingPageActivity.java
+++ app/src/main/java/com/example/atamerica/UpcomingPageActivity.java
@@ -6,39 +6,45 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.view.View;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class UpcomingPageActivity extends AppCompatActivity {
 
-//    BottomNavigationView bottomNavigationView;
+    BottomNavigationView bottomNavigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_upcoming_page);
 
-//        bottomNavigationView = findViewById(R.id.bottomNavigationView);
-//        bottomNavigationView.setSelectedItemId(R.id.upcoming);
-//
-//        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
-//            @Override
-//            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-//                switch (item.getItemId()) {
-//                    case R.id.home:
-//                        startActivity(new Intent(getApplicationContext(), HomePageActivity.class));
-//                        overridePendingTransition(0, 0);
-//                        return true;
-//                    case R.id.upcoming:
-//                        return true;
-//                    case R.id.archived:
-//                        startActivity(new Intent(getApplicationContext(), ArchivePageActivity.class));
-//                        overridePendingTransition(0, 0);
-//                        return true;
-//                }
-//
-//                return false;
-//            }
-//        });
+        bottomNavigationView = findViewById(R.id.bottomNavigationView);
+        bottomNavigationView.setSelectedItemId(R.id.upcoming);
+
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()) {
+                    case R.id.home:
+                        startActivity(new Intent(getApplicationContext(), HomePageActivity.class));
+                        overridePendingTransition(0, 0);
+                        return true;
+                    case R.id.upcoming:
+                        return true;
+                    case R.id.archived:
+                        startActivity(new Intent(getApplicationContext(), ArchivePageActivity.class));
+                        overridePendingTransition(0, 0);
+                        return true;
+                }
+
+                return false;
+            }
+        });
+    }
+
+    public void moveToProfile(View view) {
+        Intent intent = new Intent(UpcomingPageActivity.this, ProfilePageActivity.class);
+        startActivity(intent);
     }
 }
