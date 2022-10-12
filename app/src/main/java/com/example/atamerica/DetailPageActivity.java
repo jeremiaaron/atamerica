299e901cfcfd46f23a1043c8d39dc56bc14e3f9a
Erwin <septianto.mes@gmail.com>
2022-10-12 23:46:56 +0700

    Ini punya ku ya, tolong di review
    Still confused how to implement back navigation in top and implement "Read More" feature

---
+++ app/src/main/java/com/example/atamerica/DetailPageActivity.java
@@ -0,0 +1,13 @@
+package com.example.atamerica;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Button;
+
+public class DetailPageActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detail_page);
+    }
+}
