052544c556cfc4759aa38a55fcddcc5566f453cb
Raisa <raisa.imani.sani@gmail.com>
2022-11-19 17:18:57 +0700

    ALready implement the selected picture, but don't know how to make it permanent. Is it need to implemented to database?

--- app/src/main/java/com/example/atamerica/ui/profile/ProfileFragment.java
+++ app/src/main/java/com/example/atamerica/ui/profile/ProfileFragment.java
@@ -1,27 +1,32 @@
 package com.example.atamerica.ui.profile;
 
+import static android.app.Activity.RESULT_OK;
+
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.content.pm.ActivityInfo;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.ImageView;
 
 import androidx.fragment.app.Fragment;
 
 import com.example.atamerica.AuthenticateActivity;
 import com.example.atamerica.R;
 import com.example.atamerica.databinding.FragmentProfileBinding;
-import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class ProfileFragment extends Fragment {
 
     private FragmentProfileBinding binding;
 
     private Button buttonLogout;
+    private ImageView ProfilePhoto;
+    private int SELECT_PICTURE=200;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -42,6 +47,15 @@ public class ProfileFragment extends Fragment {
             LogOut();
         });
 
+        ProfilePhoto = (ImageView) mView.findViewById(R.id.profile_picture);
+
+        ProfilePhoto.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                imageChooser();
+            }
+        });
+
         return mView;
     }
 
@@ -53,4 +67,28 @@ public class ProfileFragment extends Fragment {
         startActivity(intent);
         getActivity().finish();
     }
+
+    void imageChooser(){
+        Intent i = new Intent();
+
+        i.setType("image/*");
+        i.setAction(Intent.ACTION_GET_CONTENT);
+
+        startActivityForResult(Intent.createChooser(i, "Select Picture"), SELECT_PICTURE);
+    }
+
+    public void onActivityResult(int requestCode, int resultCode, Intent data){
+        super.onActivityResult(requestCode,resultCode, data);
+
+        if (resultCode == RESULT_OK) {
+            if (requestCode == SELECT_PICTURE){
+                Uri selectedImageUri = data.getData();
+                if (null != selectedImageUri) {
+                    ProfilePhoto.setImageURI(selectedImageUri);
+                }
+            }
+
+        }
+    }
+
 }
